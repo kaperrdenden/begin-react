2522ad808a0714a33b9937421ac8a85ec96046d5
kaperrdenden <weather2sogood@gmail.com>
2022-01-08 23:38:47 +0900

    React function Component -->>>>> Class Component

--- src/Hello.js
+++ src/Hello.js
@@ -1,38 +1,20 @@
-import React from 'react';
-function Hello({color,name,isSpecial}){
-    //@1 비구조화 할당, 구조분해 할당
-
-
-
-
-
-
-
-
-    return(
-    
-    
-    <div style={{color:color}}>
-        {/* {isSpecial===true
-        ?<b>*</b>
-        :null
-        null뿐만 아니라 undefined,false렌더링하면
-        아무것도 안나옴
-        } */}
-        {isSpecial&&<b>*</b>}
-        안녕하세요{name}</div>
-    
-    )
-
-
-
-
-}
-//@2 프롭스의 디폴트값정하기 
-Hello.defaultProps = {
+import React, {Component} from 'react';
+class Hello extends Component{
+   
+   static defaultProps = {
     name:"이름없음",
-}
+   }
+    render(){
+        const {color,name,isSpecial} = this.props
+        return(
+            <div style={{color}}>
 
+                {isSpecial && <b>*</b>}
+                안녕하세요{name}
+            </div>
+        )
+    }
+}
 
 
 export default Hello;
