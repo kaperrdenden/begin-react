ff63f8862cd6b4e3eb8e85e1c3bb76680b97a7a1
kaperrdenden <weather2sogood@gmail.com>
2022-01-10 23:56:32 +0900

    function vs class

---
+++ src/Classcomponent.js
@@ -0,0 +1,38 @@
+// function vs class
+import React from 'react';
+
+function Hello({ color, name, isSpecial }) {
+  return (
+    <div style={{ color }}>
+      {isSpecial && <b>*</b>}
+      안녕하세요 {name}
+    </div>
+  );
+}
+
+Hello.defaultProps = {
+  name: '이름없음'
+};
+
+export default Hello;
+
+// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+import React, { Component } from 'react';
+
+class Hello extends Component {
+  static defaultProps = {
+    name: '이름없음'
+  };
+  render() {
+    const { color, name, isSpecial } = this.props;
+    return (
+      <div style={{ color }}>
+        {isSpecial && <b>*</b>}
+        안녕하세요 {name}
+      </div>
+    );
+  }
+}
+
+export default Hello;
