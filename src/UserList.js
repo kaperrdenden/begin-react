adafcb1671d42afb267bab093d4dd5e91b7c9ba8
kaperrdenden <weather2sogood@gmail.com>
2022-02-10 23:56:22 +0900

    users의 데이터를 렌더링해줄 User 컴포넌트 만들기

--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,15 @@
 import React, { useContext, useEffect } from 'react';
 import { UserDispatch } from './App';
 
+function User({user}) {
+    return(
+   
+             <div>
+                <b>{user.username}</b> <span>({user.email})</span>
+            </div>
+      
+    )
+}
 
 function UserList() {
 
@@ -25,15 +34,10 @@ function UserList() {
     return(
 
         <>
-            <div>
-                <b>{users[0].username}</b> <span>({users[0].email})</span>
-            </div>
-            <div>
-                <b>{users[1].username}</b> <span>({users[1].email})</span>
-            </div>
-            <div>
-                <b>{users[2].username}</b> <span>({users[2].email})</span>
-            </div>
+           <User user={users[0]} />
+           <User user={users[1]} />
+           <User user={users[2]} />
+         
         </>
         
     )
