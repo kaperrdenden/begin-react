e6bd763e6da88224bd523328e639939d141d0e47
kaperrdenden <weather2sogood@gmail.com>
2022-02-10 23:30:43 +0900

    onChange, onReset 기능 만들고 Input에 value값 지정해주기ㅏㅏ

--- src/InputSample.js
+++ src/InputSample.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { useRef, useState } from 'react/cjs/react.development';
 
 function InputSample(){
-   
+   const [text, setText] = useState('');
+   const onChange = (e) => {
+       setText(e.target.value);
+   };
+   const onReset = () => {
+       setText('');
+   }
     return(
-
+        <>
+            <input 
+                onChange={onChange}
+                value={text}
+            />
+            <button onClick={onReset}>초기화</button>
+            <div>
+                <b>값:{text}</b>
+            </div>
+        </>
     )
 }
 export default InputSample;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import InputSample from './InputSample';
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <InputSample />
   </React.StrictMode>,
   document.getElementById('root')
 );
