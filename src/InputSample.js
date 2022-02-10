2b2c6721a969e46e653f13eff48a81cb1cdf3183
kaperrdenden <weather2sogood@gmail.com>
2022-02-10 23:43:13 +0900

    여러개의 input을 하나의 useState로 관리

--- src/InputSample.js
+++ src/InputSample.js
@@ -2,22 +2,47 @@ import React from 'react';
 import { useRef, useState } from 'react/cjs/react.development';
 
 function InputSample(){
-   const [text, setText] = useState('');
+   const [inputs, setInputs] = useState({
+       name:'',
+       nickname:'',
+   });
+
+   const {name, nickname} = inputs; 
+
    const onChange = (e) => {
-       setText(e.target.value);
+        const { value, name } = e.target;
+        setInputs({
+            ...inputs,
+            [e.target.name] : value,
+            // [그냥 value,]
+        });
    };
    const onReset = () => {
-       setText('');
+       setInputs({
+           name:'',
+           nickname:'',
+       });
    }
     return(
         <>
             <input 
+                name="name" 
+                placeholder='이름'
+                value={name}
                 onChange={onChange}
-                value={text}
-            />
+                />
+            <input 
+                name="nickname" 
+                placeholder='닉네임' 
+                value={nickname} 
+                onChange={onChange}
+                />
             <button onClick={onReset}>초기화</button>
             <div>
-                <b>값:{text}</b>
+                <b>값:
+                    {name}
+                    ({nickname})
+                    </b>
             </div>
         </>
     )
