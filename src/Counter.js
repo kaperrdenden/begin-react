86f3a1e81df21745eda484425955e3310dad3353
kaperrdenden <weather2sogood@gmail.com>
2022-02-15 00:28:41 +0900

    Counter.js 초기화

--- src/Counter.js
+++ src/Counter.js
@@ -1,34 +1,14 @@
 import React from 'react';
 import { useReducer, useState } from 'react/cjs/react.development';
 
-function reducer(state,action){
-    switch(action.type){
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state - 1;
-        default:
-            return state;
-        }
-}
+
 
 function Counter(){
-    const [number,dispatch] = useReducer(reducer,0);
-
-    const onIncrease = ()=>{
-    //    setNumber((pre)=>{return pre+1});
-        dispatch({type:'INCREMENT'});
-      }
-      const onDecrease = () => {
-        // setNumber(number-1);
-        dispatch({type:'DECREMENT'});
-      }
+   
     return(
 
         <div>
-            <h1>{number}</h1>
-            <button onClick={onIncrease}>+1</button>
-            <button onClick={onDecrease}>-1</button>
+         
         </div>
     )
 }
