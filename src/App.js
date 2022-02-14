2ffa4bae73e2c6afa078c3aab056fb451afddf63
kaperrdenden <weather2sogood@gmail.com>
2022-02-15 00:55:36 +0900

    App.js useReducer로 재구현하기 위해 초기화

--- src/App.js
+++ src/App.js
@@ -12,103 +12,47 @@ function countActiveUsers(users){
   return users.filter(user => user.active).length;
 }
 
-
-function App() {
-  const [inputs, setInputs] = useState({ // setInputs를 통해서 input창을 다룬다
+const initialState = {
+  inputs:{
     username:'',
     email:'',
-  })
-  const {username, email} = inputs;
-
-  const onChange = useCallback(
-    e => {
-      const {name, value} = e.target;
-      setInputs(prevInputs => {
-        return {
-          ...prevInputs,
-          [name]: value,
-        }
-      })
-    }, [inputs]
-  )
-
-  const [users, setUsers] = useState([
+  },
+  users: [
     {
-        id:1,
-        username: 'Dong',
-        email: 'weather2sogood@gmail.com',
-        active:true,
+      id:1,
+      username: 'Dong',
+      email: 'weather2sogood@gmail.com',
+      active:true,
     },
 
-    {
-        id:2,
-        username: 'hee',
-        email: 'ssmail.com',
-        active:false,
-    },
-    {
-        id:3,
-        username: 'kang',
-        email: 'wdsa2ssssmail.com',
-        active:false,
+   {
+      id:2,
+      username: 'hee',
+      email: 'ssmail.com',
+      active:false,
+   },
+   {
+      id:3,
+      username: 'kang',
+      email: 'wdsa2ssssmail.com',
+      active:false,
     }
-]);
-  
-  
-  const nextId = useRef(4);
-  
+  ]
+};
 
-  const onCreate = useCallback(() => {
-    const user = {
-      id:nextId.current,
-      username, //onChnage에서 username이라는 state는 인풋창에 있는 값으로 실시간 매칭됨
-      email
-    };
-    setUsers(users.concat(user));
+function App() {
 
-    setInputs({
-      username:'',
-      email:'',
-    })
-    nextId.current += 1;
-  },[users, username, email]) 
-  const onRemove = useCallback(
-    id => {
-      setUsers(users.filter((user)=>{
-        return user.id !== id
-      }))
-    },[users]
-  ) 
-  const onToggle = useCallback( (id) => {
-    setUsers(
-      users.map((user) => { 
-        //map은 주어진 배열의 값을 재정의 할 때 사용하는 방법이다.
-        //users의 각 엘리먼트들의 값을 재정의하고 재정의된 그룹의 배열을 반환한다
-        return id === user.id ? {...user, active: !user.active} : user
-      })
-    )
-  
-   }
-    ,[users])
-  const count = useMemo(() =>
-   countActiveUsers(users)
-  ,[users]) 
-  
  
   return (
     <>
       <CreateUser 
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
+      
       />
       <UserList 
-        users={users}
-        onRemove={onRemove}  
-        onToggle={onToggle}
+        users={[]} 
       />
-      <div>활성사용자 수: {count}</div>
+       
+      <div>활성사용자 수: 0</div>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ import App from './App';
 import InputSample from './InputSample';
 import UserList from './UserList';
 import TestApp from './TestApp';
-import Counter from './Counter';
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
