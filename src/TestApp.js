9fae762fccb9033d673fbe738a48c753407abab9
kaperrdenden <weather2sogood@gmail.com>
2022-02-14 22:38:01 +0900

    useMemo 사용한 새 예제

--- src/TestApp.js
+++ src/TestApp.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { useState } from 'react/cjs/react.development';
+import { useMemo, useState } from 'react/cjs/react.development';
 
 const SortedWords = ({words}) => {
     const sortWords = () => {
         console.log("sortWords");
-        // delay(500);
+        
         return words.sort();
     }
     
-    const sortedWords = sortWords();
+    const sortedWords = useMemo(sortWords ,[words]) 
+    // 이렇게 함수 통으로 넘겨줘도 된다.
+
+
+    // useMemo가 없을 경우에는 input창에 글자를 쓰면 -> word라는 state가 변경됨
+    // -> word라는 state를 가진 TestApp 컴포넌트가 리렌더링됨 -> TestApp의 자식 컴폰넌트인
+    // SortedWords가 리렌더링 됨 -> sortWords();도 매번 실행됨 
+    // 근데 아무리 input창의 값이 바뀌어 봤자 sortWords();의 결과는 바뀌지 않으므로
+    // 이 상황 최적화 필요
 
     return(
         <>
@@ -29,21 +37,21 @@ const TestApp = () => {
     const [word, setWord] = useState("");
 
     const handleClick = () => {
-        console.log(word,"hadn");
+       
         setWords(words.concat(word));
         setWord("");
-        // console.log(words);
+        
     };
     return(
         <>
             <h1>React Hooks: useMemo</h1>
             <div>
-                {/* dasdsa */}
+                
                 <SortedWords  words={words}/>
             </div>
             <input 
                 value={word}
-                onChange={({target: {value}}) => { 
+                onChange={(  {target: {value}} ) => { 
                     console.log(word);
                     setWord(value);
                     console.log(word);
