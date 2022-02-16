d264153bd48418a9351e50e3b16eb6986785b1ef
kaperrdenden <weather2sogood@gmail.com>
2022-02-16 23:52:07 +0900

    immer 사용하기 전으로 되돌리기

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import InputSample from './InputSample';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 import useInputs from './hooks/useInputs';
-import produce from 'immer';
+
 function countActiveUsers(users){
   console.log('활성 사용자 수를 세는 중');
   return users.filter(user => user.active).length;
@@ -36,42 +36,50 @@ const initialState = {
       username: 'kang',
       email: 'wdsa2ssssmail.com',
       active:false,
-    },
-    {
-      id:4,
-      username: 'kg',
-      email: 'sdasdsasda2ssssmaiasdasdadsdasdl.com',
-      active:false,
     }
   ]
 };
 
-function reducer(state, action) {
-  switch (action.type) {
-    case 'CREATE_USER':
-      return produce(state, draft => {
-        draft.users.push(action.user);
-      });
-    case 'TOGGLE_USER':
-      return produce(state, draft => {
-        const user = draft.users.find(user => user.id === action.id);
-        user.active = !user.active;
-      });
-    case 'REMOVE_USER':
-      return produce(state, draft => {
-        const index = draft.users.findIndex(user => user.id === action.id);
-        draft.users.splice(index, 1);
-      });
-    default:
-      return state;
-  }
+function reducer(state, action){
+  switch (action.type){
+    case "CHANGE_INPUT":
+      return {
+        ...state,
+        inputs:{
+          ...state.inputs,
+          [action.name]:action.value,
+
+        }
+      }
+    case "CREATE_USER":
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user),
+      }
+    case "TOGGLE_USER":
+      return {
+        ...state,
+        users: state.users.map((user) => {
+          return user.id === action.id ? {...user, active:!user.active} 
+          : user
+        })
+      }
+    case "REMOVE_USER":
+        return {
+          ...state,
+          users: state.users.filter((user)=>{
+            return user.id !== action.id 
+          })
+        }
+    }
+  return state;
 }
 
 export const UserDispatch = React.createContext(null);
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const nextId = useRef(5);
+  const nextId = useRef(4);
 
   const { users } = state;
   const { username,email } = state.inputs;
